84c37fce5d5505916be570088e92bab6c9f6269e
dythirk <dythirk@github.com>
2025-12-13 07:56:05 +0000

    Test run: 2/2 passed, 0 failed, 0 skipped - 2025-12-13T07:56:04.986Z

--- src/main/java/JavalinSingleton.java
+++ src/main/java/JavalinSingleton.java
@@ -19,7 +19,7 @@ public class JavalinSingleton {
         app.get("/firstname/{first}", ctx -> {
             
             //write code here
-            ctx.result(first);
+            Object firstname = ctx.result("first");
 
         });
 
